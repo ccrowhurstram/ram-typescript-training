d1c4ecee81942c9de710010d1ef0edac33297ee0
ccrowhurstram <ccrowhurst@ramplc.com>
2016-06-23 09:03:47 +0100

    enhanced generic example

--- src/generics/promise.ts
+++ src/generics/promise.ts
@@ -2,8 +2,31 @@
 export {};
 
 /*
-let future = Q.when("Hello");
-future.then(str => {
+let futureStr = Q.when("Hello");
+futureStr.then(str => {
 	console.log(str.length);
 });
+
+let futureNum = Q.when(10);
+futureNum
+	.then(num => {
+		if (num > 10) {
+			console.log("_");
+		}
+		return num;
+	})
+	.then(num => num.toString())
+	.then(str => Array.from(str))
+	.then(strArray => {
+		console.log(strArray[0]);
+	});
+
+let futureFuture = Q.when("World");
+futureFuture
+	.then(str => new Promise(str.length))
+	.then(num => {
+		if (num > 10) {
+			console.log("_");
+		}
+	});
 */
