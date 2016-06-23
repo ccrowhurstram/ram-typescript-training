018b5b8ad900f1b71336801fa1e6d1e97e4ed170
ccrowhurstram <ccrowhurst@ramplc.com>
2016-06-23 09:05:55 +0100

    generic answers

--- src/generics/promise.ts
+++ src/generics/promise.ts
@@ -1,7 +1,29 @@
 // make sure not to pollute global scope
 export {};
 
-/*
+class Q {
+	static when<T>(val: T) {
+		return new Promise(val);
+	}
+}
+
+class Promise<T> {
+	constructor(private val: T) { }
+	then<R>(success: (val: T) => R | Promise<R>): Promise<R> {
+		const result = success(this.val);
+		if (this.isPromiseLike(result)) {
+			return result;
+		}
+		else {
+			return new Promise<R>(result);
+		}
+	}
+	private isPromiseLike<R>(val: any): val is Promise<R> {
+		return val && Reflect.has(val, "then");
+	}
+}
+
+
 let futureStr = Q.when("Hello");
 futureStr.then(str => {
 	console.log(str.length);
@@ -29,4 +51,3 @@ futureFuture
 			console.log("_");
 		}
 	});
-*/
